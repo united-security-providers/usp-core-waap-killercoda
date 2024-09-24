a60a4cfcfc032452850847652a5007c944f1c6a1
Rolf <10861950+usp-rol@users.noreply.github.com>
2024-09-24 13:06:56 +0200

    fix order of console clear to not remove user info message

--- usp-core-waap-basic/step1/foreground.sh
+++ usp-core-waap-basic/step1/foreground.sh
@@ -2,9 +2,9 @@
 
 echo "waiting for juiceshop deployment being ready..."
 while [ ! -f /tmp/.juiceshop-port-forward-pid ]; do
-  sleep 5
-  echo "...please wait for deployment to be ready and accessible via browser..."
   clear
+  echo "...please wait for deployment to be ready and accessible via browser..."
+  sleep 5
 done
 clear
 echo "...deployment ready you should now be able to access the web app using the link on the left pane"
