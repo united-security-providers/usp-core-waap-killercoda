united-security-providers/usp-core-waap-killercoda

commit 9dcb54b8606743125cff4fa956c606f0f6a4ecbc

move port-forward setup to step1 foreground

usp-core-waap-basic/intro/background.sh
@@ -12,9 +12,6 @@ echo "$(date) : waiting for juiceshop pod to be ready..."
 kubectl wait pods juiceshop -n juiceshop --for='condition=Ready' --timeout=300s
 echo "$(date) : wait ${WAIT_SEC}s..."
 sleep $WAIT_SEC
-echo "$(date) : setting up kubectl port forwarding..."
-nohup kubectl port-forward -n juiceshop svc/juiceshop 80:8000 --address 0.0.0.0 >/dev/null &
-echo $! > /tmp/.juiceshop-port-forward-pid
 echo "$(date) : juiceshop setup finished"
 # setup core waap operator
 # variables

usp-core-waap-basic/step1/foreground.sh
@@ -6,5 +6,8 @@ while [ ! -f /tmp/.juiceshop-port-forward-pid ]; do
   echo "...please wait for deployment to be ready and accessible via browser..."
   sleep 5
 done
+sleep 3
+nohup kubectl port-forward -n juiceshop svc/juiceshop 80:8000 --address 0.0.0.0 >/dev/null &
+echo $! > /tmp/.juiceshop-port-forward-pid
 clear
 echo "...deployment ready you should now be able to access the web app using the link on the left pane"

commit 63ba3b901ec2c7330b7c9738fef0a9ef8987c3db

fixes bug introduced in 9dcb54b8606743125cff4fa956c606f0f6a4ecbc

usp-core-waap-basic/intro/background.sh
@@ -4,6 +4,7 @@
 
 # variables
 WAIT_SEC=15
+JUICESHOP_SETUP_FINISH="/tmp/.juiceshop-finished"
 
 # setup juiceshop web app
 echo "$(date) : applying juiceshop web app..."
@@ -12,6 +13,7 @@ echo "$(date) : waiting for juiceshop pod to be ready..."
 kubectl wait pods juiceshop -n juiceshop --for='condition=Ready' --timeout=300s
 echo "$(date) : wait ${WAIT_SEC}s..."
 sleep $WAIT_SEC
+touch $JUICESHOP_SETUP_FINISH && echo "$(date) : wrote file $JUICESHOP_SETUP_FINISH to indicate juicesetup setup completion to foreground process"
 echo "$(date) : juiceshop setup finished"
 # setup core waap operator
 # variables

usp-core-waap-basic/step1/foreground.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "waiting for juiceshop deployment being ready..."
-while [ ! -f /tmp/.juiceshop-port-forward-pid ]; do
+while [ ! -f /tmp/.juiceshop-finished ]; do
   clear
   echo "...please wait for deployment to be ready and accessible via browser..."
   sleep 5
 done
-sleep 3
+sleep 1
 nohup kubectl port-forward -n juiceshop svc/juiceshop 80:8000 --address 0.0.0.0 >/dev/null &
 echo $! > /tmp/.juiceshop-port-forward-pid
 clear