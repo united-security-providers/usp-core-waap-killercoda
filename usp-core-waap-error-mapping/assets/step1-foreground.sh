aedf706af73b66d36df97732f6f374d827b1adbf
Rolf <10861950+usp-rol@users.noreply.github.com>
2024-12-03 16:32:57 +0100

    fix error-mapping step2 removal

--- usp-core-waap-error-mapping/assets/step1-foreground.sh
+++ usp-core-waap-error-mapping/assets/step1-foreground.sh
@@ -4,8 +4,20 @@ rm $0
 
 clear
 
-echo -n "Installing scenario..."
-while [ ! -f /tmp/.backend-finished ]; do
+echo -n "Installing Juice Shop backend..."
+while [ ! -f /tmp/.backend_installed ]; do
+  echo -n '.'
+  sleep 1;
+done;
+echo " done"
+echo -n "Installing USP Core WAAP Operator..."
+while [ ! -f /tmp/.operator_installed ]; do
+  echo -n '.'
+  sleep 1;
+done;
+echo " done"
+echo -n "Configuring CoreWaapService instance..."
+while [ ! -f /tmp/.waap_installed ]; do
   echo -n '.'
   sleep 1;
 done;
