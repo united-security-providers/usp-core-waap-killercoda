044396e29e66b0c84efbd34dac40e3768c6544be
Marcel Schoen <marcel.schoen@united-security-providers.ch>
2025-01-17 15:15:29 +0000

    Adding attacker web server for CSRF Policy demo

--- usp-core-waap-csrf-protection/intro/foreground.sh
+++ usp-core-waap-csrf-protection/intro/foreground.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-FILE=~/.scenario_staging/step1-foreground.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
+FILE=~/.scenario_staging/intro-foreground.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
