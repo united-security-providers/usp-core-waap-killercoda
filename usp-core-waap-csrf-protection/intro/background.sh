1691d9fc7bd4ec6f70957b5187b7b978d832e1d0
Marcel Schoen <marcel.schoen@united-security-providers.ch>
2025-01-20 10:58:13 +0000

    Adding attacker web server for CSRF Policy demo

--- usp-core-waap-csrf-protection/intro/background.sh
+++ usp-core-waap-csrf-protection/intro/background.sh
@@ -52,8 +52,8 @@ echo "--------------------------------------------------------"
 cat ~/.scenario_staging/$ATTACKER_POD.yaml
 echo "--------------------------------------------------------"
 
-echo "$(date) : performing sed...)"
-sed --debug 's/JUICESHOP_HOST/${JUICESHOP_HOST}/g' <~/.scenario_staging/$ATTACKER_POD.yaml >/tmp/$ATTACKER_POD.yaml
+echo "$(date) : performing sed..."
+sed 's/JUICESHOP_HOST/${JUICESHOP_HOST}/g' <~/.scenario_staging/$ATTACKER_POD.yaml >/tmp/$ATTACKER_POD.yaml
 RC=$?
 echo "$(date) : status code of sed: ${RC}"
 
