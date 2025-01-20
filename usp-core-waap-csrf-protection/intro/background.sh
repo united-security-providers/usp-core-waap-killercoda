bd2061fa8e8128f411aa76fe499ef94698a725be
Marcel Schoen <marcel.schoen@united-security-providers.ch>
2025-01-20 11:16:22 +0000

    Adding attacker web server for CSRF Policy demo

--- usp-core-waap-csrf-protection/intro/background.sh
+++ usp-core-waap-csrf-protection/intro/background.sh
@@ -50,7 +50,9 @@ JUICESHOP_HOST=`sed 's/PORT/8080/g' /etc/killercoda/host`
 echo "$(date) : juiceshop host to use in attacker form: $JUICESHOP_HOST"
 
 echo "$(date) : performing awk..."
-awk '{sub(/JUICESHOP_HOST/,"'$JUICESHOP_HOST'")}1' /root/.scenario_staging/$ATTACKER_POD.yaml > /tmp/$ATTACKER_POD.yaml
+export REPLACE='{sub(/JUICESHOP_HOST/,"${JUICESHOP_HOST}")}1'
+echo "$(date) : replace value: $REPLACE"
+awk $REPLACE /root/.scenario_staging/$ATTACKER_POD.yaml > /tmp/$ATTACKER_POD.yaml
 #YAML=`sed 's/JUICESHOP_HOST/${JUICESHOP_HOST}/g' </root/.scenario_staging/$ATTACKER_POD.yaml`
 RC=$?
 echo "$(date) : status code of awk: ${RC}"
