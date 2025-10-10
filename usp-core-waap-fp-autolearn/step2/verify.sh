dc263bdeb38997f46f9404b0d1b156430412d6a5
Rolf <10861950+usp-rol@users.noreply.github.com>
2025-10-10 15:15:37 +0200

    fix / improve autolearing scenario for dynamic cli download and socket.io detection

--- usp-core-waap-fp-autolearn/step2/verify.sh
+++ usp-core-waap-fp-autolearn/step2/verify.sh
@@ -4,6 +4,6 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-SOCKET_IO_LOGS=$(kubectl logs -n juiceshop -l app.kubernetes.io/name=usp-core-waap | grep "coraza-vm.*/socket.io" | wc -l)
+SOCKET_IO_LOGS=$(kubectl logs -n juiceshop -l app.kubernetes.io/name=usp-core-waap | grep "\[critical\]\[golang\]" | grep -E '"request.path":"/socket.io' | wc -l)
 SQLI_LOGS=$(kubectl logs -n juiceshop -l app.kubernetes.io/name=usp-core-waap --tail=-1 | grep APPLICATION-ATTACK-SQLI | wc -l)
 test $SOCKET_IO_LOGS -eq 0 && test $SQLI_LOGS -gt 1
