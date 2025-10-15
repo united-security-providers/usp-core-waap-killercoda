396d87f2c53044191a71aff5a4055a66748b7592
Rolf <10861950+usp-rol@users.noreply.github.com>
2025-10-15 08:49:10 +0200

    redmine#290697 graphql scenario updates and fixes

--- usp-core-waap-graphql/assets/step1-foreground.sh
+++ usp-core-waap-graphql/assets/step1-foreground.sh
@@ -13,15 +13,6 @@ while [ ! -f /tmp/.backend_installed ]; do
   echo -n '.'
   sleep 1;
 done;
-LLDAP_TOKEN=$(curl -s -X POST -H 'Content-Type: application/json' http://localhost:8080/auth/simple/login -d '{"username":"admin", "password":"insecure"}' | jq -r '.token')
-export LLDAP_TOKEN
-if [ -n "$LLDAP_TOKEN" ]; then
-  echo " done"
-  echo
-  echo "*** Scenario ready ***"
-else
-  echo " failed"
-  echo
-  echo "ERROR: LLDAP backend setup failed - please restart the scenario (if consistent report this error on scenario overview page)"
-  exit 1
-fi
+echo " done"
+echo
+echo "*** Scenario ready ***"
--- usp-core-waap-graphql/intro/foreground.sh
+++ usp-core-waap-graphql/intro/foreground.sh
@@ -5,3 +5,11 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 FILE=~/.scenario_staging/step1-foreground.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
+
+LLDAP_TOKEN=$(curl -s -X POST -H 'Content-Type: application/json' http://localhost:8080/auth/simple/login -d '{"username":"admin", "password":"insecure"}' | jq -r '.token')
+export LLDAP_TOKEN
+
+if [ -z "$LLDAP_TOKEN" ]; then
+  echo "ERROR: LLDAP backend setup failed - please restart the scenario (if consistent report this error on scenario overview page)"
+  exit 1
+fi
