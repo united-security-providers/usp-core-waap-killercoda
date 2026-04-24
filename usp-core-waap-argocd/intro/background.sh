12ca36475b8a8def7af67469e3e324097008b9a3
Rolf <10861950+usp-rol@users.noreply.github.com>
2026-04-24 16:12:17 +0200

    redmine#292371 initial argocd scenario content

---
+++ usp-core-waap-argocd/intro/background.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2025 United Security Providers AG, Switzerland
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+# intro background script log available at /var/log/killercoda/background0_std(err|out).log
+
+
+##################################################
+# Initialization
+##################################################
+# variables
+ARGOCD_NAMESPACE="argocd"
+GOGS_NAMESPACE="gogs"
+GOGS_USER="gituser"
+GOGS_PASSWORD="gitpassword"
+GOGS_EMAIL="gituser@gogs.local"
+BACKEND_SETUP_FINISH="/tmp/.backend_installed"
+
+echo "$(date) : change to scenario_staging dir..."
+cd ~/.scenario_staging/ || exit 1
+
+##################################################
+# Part 1: setup argocd backend application
+##################################################
+echo "$(date) : installing argocd..."
+# install argocd stable into kubernets
+kubectl create namespace ${ARGOCD_NAMESPACE} || exit 1
+kubectl apply -n ${ARGOCD_NAMESPACE} --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# install argocd cli
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd || exit 1
+rm argocd-linux-amd64
+
+# wait for argocd k8s install to be ready
+kubectl -n ${ARGOCD_NAMESPACE} wait --all --for=condition=Ready --timeout 300s pod
+
+# argocd cli login
+kubectl config set-context --current --namespace=${ARGOCD_NAMESPACE}
+argocd login --core || exit 1
+
+# make argocd UI accessible
+# reconfiguring argocd to insecure mode is required to access http port (for killercoda for example)
+kubectl patch configmap argocd-cmd-params-cm -n ${ARGOCD_NAMESPACE} \
+  --type merge \
+  -p '{"data":{"server.insecure":"true"}}'
+kubectl rollout restart deployment argocd-server -n ${ARGOCD_NAMESPACE}
+
+# wait for argocd k8s install to be ready
+kubectl -n ${ARGOCD_NAMESPACE} wait --all --for=condition=Ready --timeout 300s pod
+
+# patch svc to be of type NodePort and access via port 30081
+kubectl patch svc argocd-server -n ${ARGOCD_NAMESPACE} -p '{"spec":{"type":"NodePort","ports":[{"port":80, "nodePort":30081}]}}'
+
+##################################################
+# Part 2: setup gogs backend application
+##################################################
+
+echo "$(date) : installing gogs..."
+# apply gogs manifests
+kubectl create namespace ${GOGS_NAMESPACE} || exit 1
+kubectl apply -n ${GOGS_NAMESPACE} -f ./gogs.yaml
+
+# wait for gogs k8s install to be ready
+kubectl wait pods -l app=gogs -n ${GOGS_NAMESPACE} --for='condition=Ready' --timeout=300s
+
+# create initial gogs user and repo via gogs API
+kubectl exec -n ${GOGS_NAMESPACE} deployment/gogs -- /app/gogs/gogs admin create-user --name ${GOGS_USER} --password ${GOGS_PASSWORD} --email ${GOGS_EMAIL}
+
+##################################################
+# Finalization: signal setup complete to foreground script
+##################################################
+touch $BACKEND_SETUP_FINISH && echo "$(date) : wrote file $BACKEND_SETUP_FINISH to indicate backend setup completion to foreground process"
+echo "$(date) : backend setup finished"
---
+++ usp-core-waap-argocd/step1/info.md
@@ -0,0 +1,48 @@
+<!--
+SPDX-FileCopyrightText: 2025 United Security Providers AG, Switzerland
+
+SPDX-License-Identifier: GPL-3.0-only
+-->
+
+&#127919; In this step you will:
+
+* Access Gogs webUI
+* Access ArgoCD webUI
+
+### Access Gogs webUI
+
+In this scenario the [Gogs](https://gogs.io/) application has been setup and will be used as a local code repository managing the actual applications to be deployed.
+
+Access the application using the following information:
+
+* WebUI: {{TRAFFIC_HOST1_30080}}
+* Username: `gituser`
+* Password: `gitpassword`
+
+### Access ArgoCD webUI
+
+> &#8987; Wait until the console on the right side shows `*** Scenario ready ***` before accessing the backend (otherwise you'll see an `HTTP 502 Bad Gateway` error)!
+
+In this scenario the [Argo CD](https://argo-cd.readthedocs.io/) application has been setup and will be used to deploy and maintain application in a Kubernetes cluster.
+
+Access the application using the following information:
+
+* WebUI: {{TRAFFIC_HOST1_30081}}
+* Username: admin
+* Get initial Password using command below
+
+```shell
+argocd admin initial-password -n argocd
+```{{exec}}
+
+<details>
+<summary>example command output</summary>
+
+```shell
+
+```
+
+</details>
+<br />
+
+> &#10071; Verify that you can login to the [Argo CD]({{TRAFFIC_HOST1_30081}}) and [Gogs]({{TRAFFIC_HOST1_30080}}) webUI!
