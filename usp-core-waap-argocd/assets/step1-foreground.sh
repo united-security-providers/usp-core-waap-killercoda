ff7d5702a5ef24e6a3923960e27432044b47d1e3
Rolf <10861950+usp-rol@users.noreply.github.com>
2026-04-28 15:29:46 +0200

    redmine#292371 argocd scenario fix foreground script output

--- usp-core-waap-argocd/assets/step1-foreground.sh
+++ usp-core-waap-argocd/assets/step1-foreground.sh
@@ -12,36 +12,40 @@ BACKEND_SETUP_WAAP_OPERATOR="/tmp/.backend_corewaap_operator_installed"
 BACKEND_SETUP_DEMO_APP="/tmp/.backend_demo_app_installed"
 BACKEND_SETUP_FINISH="/tmp/.backend_installed"
 
-log_info() {
+log_install() {
   echo
   echo -n "$1"
 }
 
+log_info() {
+  echo -e "$1"
+}
+
 clear
 
-log_info "Installing ArgoCD application..."
+log_install "Installing ArgoCD application"
 while [ ! -f ${BACKEND_SETUP_ARGOCD} ]; do
   echo -n '.'
   sleep 1;
 done;
-log_info "Installing Gogs application..."
+log_install "Installing Gogs application"
 while [ ! -f ${BACKEND_SETUP_GOGS} ]; do
   echo -n '.'
   sleep 1;
 done;
-log_info "Installing WAAP Operator..."
+log_install "Installing WAAP Operator"
 while [ ! -f ${BACKEND_SETUP_WAAP_OPERATOR} ]; do
   echo -n '.'
   sleep 1;
 done;
-log_info "Installing Demo application..."
+log_install "Installing Demo application"
 while [ ! -f ${BACKEND_SETUP_DEMO_APP} ]; do
   echo -n '.'
   sleep 1;
 done;
-log_info "Finalizing scenario setup..."
+log_install "Finalizing scenario setup"
 while [ ! -f ${BACKEND_SETUP_FINISH} ]; do
   echo -n '.'
   sleep 1;
 done;
-log_info "*** Scenario ready ***"
+log_info "\n\n*** Scenario ready ***"
